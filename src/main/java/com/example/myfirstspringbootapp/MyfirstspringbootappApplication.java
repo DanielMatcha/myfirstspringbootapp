72b49a72101e4d22a91e84195a9fa14bc3fa2e3b
DanielMatcha <100127870+DanielMatcha@users.noreply.github.com>
2022-09-13 23:45:01 +0530

    Update MyfirstspringbootappApplication.java

--- src/main/java/com/example/myfirstspringbootapp/MyfirstspringbootappApplication.java
+++ src/main/java/com/example/myfirstspringbootapp/MyfirstspringbootappApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class MyfirstspringbootappApplication {  
+public class MyfirstspringbootappApplication { 
 
 	public static void main(String[] args) { 
 		SpringApplication.run(MyfirstspringbootappApplication.class, args);
